ab55fecad19bea53e3c526a80573390f15dd44c4
Tobias Hallingstad <tobias@hallingstad.me>
2021-01-19 16:51:50 +0100

    Started work on WikiAPI2

--- WikipediaAPI.py
+++ WikipediaAPI.py
@@ -1,14 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import pretty_errors
+import xmltodict as xd
+
+import re
+
+import lxml
+import xmltodict
+from collections import OrderedDict
 
 #page = requests.get('https://en.wikipedia.org/wiki/CD_Projekt')
 #page = requests.get('https://en.wikipedia.org/wiki/FromSoftware')
-#page = requests.get('https://en.wikipedia.org/wiki/Ubisoft')
+page = requests.get('https://en.wikipedia.org/wiki/Ubisoft')
 #page = requests.get('https://en.wikipedia.org/w/index.php?search=Ubisoft')
 #page = requests.get('https://en.wikipedia.org/w/index.php?search=Valve_Corporation')
 #page = requests.get('https://en.wikipedia.org/w/index.php?search=Cryteck_Studios')
-page = requests.get('https://en.wikipedia.org/w/index.php?search=Crytek')
+#page = requests.get('https://en.wikipedia.org/w/index.php?search=Crytek')
 
 
 def searchForWikiPage(searchText):
@@ -39,6 +46,10 @@ def getWikiData(page):
     for i in range(len(texts)):
         text = texts[i].lower()
 
+        if text == 'founders':
+            print(text)
+            
+
         if text == 'headquarters':
             dataOut[text] = texts[i + 1], texts[i + 3]
 
@@ -57,9 +68,58 @@ def getWikiData(page):
 
     return dataOut
 
-#page = searchForWikiPage('Godot_(game_engine)')
+def getWikiData2(page):
+    soup = BeautifulSoup(page.content, 'lxml')
+    results = soup.find(id='mw-content-text')
+    job_elems = results.find('table', class_='infobox vcard')
+    job_data = job_elems.findAll('tr')
 
-#print(getWikiData(page))
+    #print(job_data[6].findAll(text=True))
+
+    dataOut = {
+        'Type': None,
+        'Industry': None,
+        'Founded': None,
+        'Founder': None,
+        'Founders': None,
+        'Headquarters': None,
+        'Area served': None,
+        'Number of employees': None,
+        'Parent': None,
+        'Native name': None,
+        'Romanized': None,
+        'Website': None
+    }
+
+    for line in job_data:
+        oLine = line
+        line = line.findAll(text=True)
+
+        if not line:
+            continue
+
+        if line[0] == 'Founded':
+            dataOut[line[0]] = line[len(line) - 2]
+
+        elif line[0] == 'Headquarters':
+            dataOut[line[0]] = ''
+            city = [line[i + 1] for i in range(len(line) - 1)] if len(line) > 3 else line[1]
+
+            for part in city:
+                dataOut[line[0]] += part
+
+        elif line[0] == 'Number of employees':
+            split =
+            #dataOut[line[0]] = line[1].split('\\')
+
+        elif line[0] in dataOut:
+            dataOut[line[0]] = [line[i + 1] for i in range(len(line) - 1)] if len(line) > 3 else line[1]
+
+    return dataOut
+
+#page = searchForWikiPage('Activision')
+
+print(getWikiData2(page))
 
 
 # -----------
